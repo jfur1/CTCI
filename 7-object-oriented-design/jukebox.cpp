90bc4f69f55541f986c6fa3b3933824c559db33a
John Furlong <johnsdump1@gmail.com>
2020-06-13 13:09:47 -0600

    daily commit

--- 7-object-oriented-design/jukebox.cpp
+++ 7-object-oriented-design/jukebox.cpp
@@ -5,36 +5,60 @@ Description: Jukebox
     Design a musical jukebox using object oriented principles.
 */
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
 struct Song{
     string title;
     string artist;
+    string album;
+    string imgFilename;
     int length; // Duration in seconds
+};
+
+struct Playlist{
+    string name;
+    queue<Song> tracks;
+};
+
+struct User{
+    string name;
+    Playlist playlist;
 };
 
 class Jukebox{
     private:
-        vector<Song> songs;
+        queue<Song> q;
+        vector<Song> library;
+        Song currentSong;
     public:
+
         Jukebox(){
-
-        }
-
-        void display(){
-
-        }
-
-        void chooseSong(){
             
         }
 
-        void queueSong(){
-
+        void display(Song song){
+            // print(/songs/...)
+            cout << song.title << endl;
+            cout << song.artist << endl;
+            cout << song.album << endl;
+            cout << song.length << endl;
         }
 
-        void addSongToLibrary(){
+        void chooseSong(Song song){
+            if(q.empty())
+                currentSong = song;
+            else
+                queueSong(song);
+        }
 
+        void queueSong(Song song){
+            q.push(song);
+        }
+
+        void addSongToLibrary(Song song){
+            library.push_back(song);
+            song.imgFilename = "/songs/" + song.title;
         }
 };
