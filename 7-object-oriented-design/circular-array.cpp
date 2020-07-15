b783d625e6680be1c0e513e86731061ad87991db
John Furlong <johnfurlong24@gmail.com>
2020-07-15 13:14:20 -0600

    finishing oop section today

--- 7-object-oriented-design/circular-array.cpp
+++ 7-object-oriented-design/circular-array.cpp
@@ -2,6 +2,36 @@
 Author: John Furlong
 Date: June 12, 2020
 Description: Circular Array
-    Implement a circularArray class that supports an array-like data structure which can be eddiciently rotated. If possible, the
+    Implement a circularArray class that supports an array-like data structure which can be efficiently rotated. If possible, the
     class should use a generic type (also called a template), and should support iteration via the standard (Obj o : circularArray) notation.
 */
+#include <iostream>
+const int size = 6;
+using namespace std;
+
+class circularArray{
+    private:
+        char data[size];
+        
+    public:
+        circularArray(char arr[]){
+            for(int i = 0; i < size; i++){
+                data[i] = arr[i];
+            }
+        }
+
+        // Function rotates the circular array, starting from a given index. Uses the mod operator to perform the rotation in-place.
+        void rotate(int index){
+            for(int i = index; i < size + index; i++){
+                cout << data[(i % size)] << " ";
+            }
+        }
+};
+
+int main(){
+    // This approach takes O(n) time and O(1) space.
+    char values[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    circularArray circle(values);
+    circle.rotate(3);
+    return 0;
+}
--- 7-object-oriented-design/parking-lot.cpp
+++ 7-object-oriented-design/parking-lot.cpp
@@ -3,4 +3,39 @@ Author: John Furlong
 Date: June 12, 2020
 Description: Parking Lot
     Design a parking lot using object oriented principles.
+    Assume the following:
+        - The parking lot has multiple levels. Each level has multiple rows of spots.
+        - The parking lot can park cars, buses, and motorcycles
+        - The parking lot has motorcycle spots, compact spots, and large spots.
+        - A motorcycle can park in any spot
+        - A car can park in either a single compact spot, or a single large spot. 
+        - A bus can park only in five large spots that are consecutive and within the same row.
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Vehicle{
+    private:
+        
+    public:
+        Vehicle(){
+            
+        }
+};
+
+class Motorcycle : public Vehicle{
+    private:
+        int size;
+    
+};
+
+class ParkingSpot{
+    private:
+
+    public:
+        ParkingSpot(){
+
+        }
+
+};
