7541cb315572732811158527b102f7152d7eef28
John Furlong <johnfurlong24@gmail.com>
2020-07-16 13:18:15 -0600

    Worked on implementing move validation in othello. Need to work out corner cases and checkWin() later

--- 7-object-oriented-design/othello.cpp
+++ 7-object-oriented-design/othello.cpp
@@ -20,7 +20,7 @@ class Othello{
 
     public:
         Othello(){
-            turn = 0;
+            turn = 1;
             for(int i = 0; i < 8; i++){
                 for(int j = 0; j < 8; j++){
                     if(i == 3 && j == 3)
@@ -37,6 +37,7 @@ class Othello{
             }
         }
 
+        // Utility function prints the game board
         void printBoard(){
             char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
             cout << '\t';
@@ -53,10 +54,56 @@ class Othello{
             }
         }
 
-        void validateMove(int player, string location){
+        // Functions checks if the tile at the given location is free, and whether or not the move captures an opponent piece.
+        // Returns true if move is valid and capturing, else returns false
+        bool validateMove(int player, string move){
 
+            // Convert the player's move into row & column indexes
+            int row = int(move[0]) - 48 - 1;
+            int col = move[2] - 96 - 1;
+            // First check if spot is open
+            cout << "Validating Move. Row: " << row << ". Column: " << col << endl;
+
+            if(board[row][col] != -1){
+                cout << "Tile " << move << " has already been played. Please select a different tile\n";
+                return 0;
+            }
+            
+            // Coordinate deltas for checking adjacent tiles -- Clockwise starting from the north
+            int delta_x[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+            int delta_y[] = {0, 1, 1, 1, 0, -1, -1, -1};
+            int opponent = (player == 0) ? 1 : 0;
+
+            // For each adjacent tile, see if the opposite side is also the current color
+            for(int i = 0; i < 9; i++){
+                // Check if there is an adjacent opponent tile
+                int tmpRow = row + delta_x[i];
+                int tmpCol = col + delta_y[i];
+                if(board[tmpRow][tmpCol] == opponent){
+                    cout << "Found adjacent enemy piece at row: " << tmpRow << " and col: " << tmpCol << endl;
+                    int oppositeRow = tmpRow + delta_x[i];
+                    int oppositeCol = tmpCol + delta_y[i];
+                    cout << "Checking for enclosing piece at row: " << oppositeRow << " and col: " << oppositeCol << endl;
+                    // Is the opposite side of the adjacent-opponent piece also the current player's?
+                    if(board[oppositeRow][oppositeCol] == player){
+                        cout << "Valid Move! Captured enemy piece.\n";
+                        board[row][col] = player;
+                        board[tmpRow][tmpCol] = player;
+                        return 1;
+                    }   
+                }
+            }
+            cout << "Invalid Move!\n";
+            return 0;
         }
 
+        // Function checks if the current state of the board results in a winner/loser
+        bool checkWin(){
+
+            return 0;
+        }
+
+        // Utility function used to prompt user for their selected move
         string prompt(){
             // Prompt the player to select a row and column
             int row; char col;
@@ -65,37 +112,42 @@ class Othello{
             cout << "Enter a column letter (lowercase)" << endl;
             cin >> col;
             int col_dec = col - 96;
-            if(row < 1 || row > 8)
-                return "";
-            else if(col_dec < 1 || col_dec > 8)
-                return "";
-            else{
-                string row = to_string(row);
-                return row+col;                
-            }
-        }
 
+            // Only accept valid board positions
+            if(row < 1 || row > 8){
+                if(col_dec < 1 || col_dec > 6){
+                    cout << "Enter a row number between 1 and 8. Also, enter a column between 'a' and 'h'\n";
+                    return "";
+                }
+                cout << "Enter a row number between 1 and 8.\n";
+                return "";
+            }
+            // Concatenate the valid move
+            string move = to_string(row) + ' ' + col;
+
+            cout << "You entered: " << move << endl;
+
+            return move;
+            }
+
+        // Driver function used to play the game
         void play(){
             while(1){
                 // Dark goes first, alternate each turn.
                 turn = (turn != 0) ? 0 : 1;
                 // Print the board
                 printBoard();
-                // Prompt the player to select a row and column
-                int row; char col;
-                cout << "Enter a row number: " << endl;
-                cin >> row;
-                cout << "Enter a column letter (lowercase)" << endl;
-                cin >> col;
+                // Prompt the player to select a row and column; Continue until valid move is chosen
+                string move = prompt();
+                while(move == ""){
+                    move = prompt();
+                }
+                bool valid = validateMove(turn, move);
+                while(!valid){
+                    valid = validateMove(turn, move);
+                }
+                // Once a move has been made, check for winner
 
-                // Check if move is valid before execution
-                // if( validateMove(player, location))
-                //      board[loc] = player
-                // else ==> Loop prompt
-
-                //cout << "You entered: " << row << col << endl;
-                int col_dec = col - 96;
-                //cout << col << " in decimal: " << col_dec << endl;
 
                 break;
             }
@@ -107,7 +159,7 @@ class Othello{
 
 int main(){
     Othello game = Othello();
-    game.printBoard();
+    //game.printBoard();
 
     // Dark player goes first
     game.play();
