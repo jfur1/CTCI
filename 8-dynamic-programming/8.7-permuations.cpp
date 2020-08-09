f10fc5ef516fb06ec15f076bc0114698193f3613
John Furlong <johnfurlong24@gmail.com>
2020-08-09 08:38:18 -0600

    String permutations using recursion and backtracking

--- 8-dynamic-programming/8.7-permuations.cpp
+++ 8-dynamic-programming/8.7-permuations.cpp
@@ -5,11 +5,24 @@
 #include <iostream>
 using namespace std;
 
-void permutations(string word){
+void permutations(string str, int i, int n){
+    if(i == n - 1){
+        cout << str << endl;
+        return;
+    }
+    // Process the remaining characters of the string
+    for(int j = i; j < n; j++){
+        swap(str[i], str[j]);
 
+        permutations(str, i+1, n);
+
+        swap(str[i], str[j]);
+    }
 }
 
 int main(){
+    string str = "ABC";
 
+    permutations(str, 0, str.length());
     return 0;
 }
