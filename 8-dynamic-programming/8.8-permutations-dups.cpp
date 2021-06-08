ed31c8e2e42dcb2691951e69075ec3a53697b9b3
John Furlong <johnfurlong24@gmail.com>
2021-06-08 13:57:05 -0600

    Continue dynamic programming chapter

---
+++ 8-dynamic-programming/8.8-permutations-dups.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+    8.8: Permutations with Duplicates
+        Write a method to compute all permutations of a string whose characters are not necessarily unique. 
+        The list of permutations should not have duplicates.
+*/
+void permutations(string str){
+    
+}
+
+int main(){
+
+    return 0;
+}
